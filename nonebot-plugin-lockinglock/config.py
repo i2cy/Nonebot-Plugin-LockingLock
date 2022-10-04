56bf3dea6d7e9ddf1a59bd0eec881e539bf1efdd
Icy Cloud <i2cy@outlook.com>
2022-10-04 22:04:29 +0800

    Initial commit

---
+++ nonebot-plugin-lockinglock/config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Author: i2cy(i2cy@outlook.com)
+# Project: NonebotPluginLockingLock
+# Filename: config
+# Created on: 1/10/2022
+
+"""
+config.i2ll_host
+config.i2ll_port
+config.i2ll_psk
+config.i2ll_devices
+"""
+
---
+++ setup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Author: i2cy(i2cy@outlook.com)
+# Project: NonebotPluginLockingLock
+# Filename: setup
+# Created on: 1/10/2022
